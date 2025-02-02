8916bbd5f20c344082b304c309b64c4f73794798
Mariana Luduena <63405308+marianaluduena@users.noreply.github.com>
2025-02-02 15:12:43 -0300

    br_register.js created

---
+++ business_rules/br_register.js
@@ -0,0 +1,10 @@
+/*BUSINESS RULES: REGISTER 
+
+
+- Password should have minimum 8 characters, at least 1 uppercase letter, 1 lowercase letter and 1 number.
+
+- Gender Male or Female must be specified to send the form.
+
+
+*/ 
+
