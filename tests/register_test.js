61029d30adfb4dbc2e0d19a273c5088297f3fe6e
Mariana Luduena <marianabelenluduena@gmail.com>
2025-02-19 14:35:05 -0300

    Test case format
    - Created TC format with TC-Nº, title, prerequisites and steps.

--- tests/register_test.js
+++ tests/register_test.js
@@ -3,6 +3,20 @@ import BR_Register from "../business_rules/br_register.js";
 
 // HAPPY PATH TEST
 
+/* TC-001: Successfull account created
+
+- Verify that the user can create successfully their account with valid format data.
+
+* PREREQUISITES: None.
+
+* STEPS:
+
+- Go to the Login fucntionallity, and click in the Register button. 
+
+- Fill the form with the following data:
+
+*/
+
 let objUser = new NewUser("j-Watson-123", "j-Watson-123"); // pass and confirm pass are this way, because they don't have setters being sensitive data
 
 objUser.name = "John";
@@ -32,7 +46,21 @@ objUser.confirmPassword = "doctor-Watson";
 
 */
 
-// NEGATIVE TEST CASE: NOT MATHCING PASSWORD FORMAT
+
+
+/* TC-002: Not matching password format
+
+- Verify that the user CAN NOT create their account with invalid password format data.
+
+* PREREQUISITES: None.
+
+* STEPS:
+
+- Go to the Login fucntionallity, and click in the Register button. 
+
+- Fill the form with the following data:
+
+*/
 
 objUser = new NewUser("123", "123");
 
@@ -59,7 +87,20 @@ if (objUser.name || objUser.lastName == Number){
     }
 
 
-// NEGATIVE TEST CASE: Numbers in Name and Last Name fields
+
+/* TC-003: Numbers in Name and Last Name fields
+
+- Verify that the user CAN NOT create their account with invalid name and last name format data by entering numbers.
+
+* PREREQUISITES: None.
+
+* STEPS:
+
+- Go to the Login fucntionallity, and click in the Register button. 
+
+- Fill the form with the following data:
+
+*/
 
 objUser = new NewUser("justTest1", "justTest1");
 
